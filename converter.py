395b68bd3d36afd865d039b794d27d4f0b8b98e5
MTAC <mtactheming@gmail.com>
2020-07-10 14:45:11 -0400

    Replace hardcoded user directory with os.path

--- converter.py
+++ converter.py
@@ -1,5 +1,6 @@
 from pydub import AudioSegment
+import os
 
 def convert():
-	    sound = AudioSegment.from_mp3("/home/nya/Downloads/audio.mp3")
-	    sound.export("/home/nya/Downloads/audio.wav", format="wav") 
+	    sound = AudioSegment.from_mp3(os.path.expanduser("~/Downloads/audio.mp3"))
+	    sound.export(os.path.expanduser("~/Downloads/audio.wav", format="wav")) 
